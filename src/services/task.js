73bc4e5db608b4950a70d1801673318fc0c352ac
BaoDuyLeHoang <90242623+BaoDuyLeHoang@users.noreply.github.com>
2024-11-25 02:42:54 +0700

    Revert "change api for getting task not schduling"
    This reverts commit 84623391f7af656325ab52ec6e29f856522c71a7.

--- src/services/task.js
+++ src/services/task.js
@@ -38,7 +38,7 @@ export const getTasksByAccountId = async (accountId, date, pageIndex = 1, pageSi
         }
 
         const response = await axios.get(
-            `${BASE_URL}/Task/tasksNotScheduling/account/${accountId}?${params}`,
+            `${BASE_URL}/Task/tasks/account/${accountId}?${params}`,
             {
                 headers: {
                     Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
